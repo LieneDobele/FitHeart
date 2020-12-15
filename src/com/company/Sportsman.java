58ae2fe84faf3d33ec0a900c98dbcf585c94b1ca
KristineTaluma <75748988+KristineTaluma@users.noreply.github.com>
2020-12-15 23:17:00 +0200

    Method advices written in Sportsman class

--- src/com/company/Sportsman.java
+++ src/com/company/Sportsman.java
@@ -68,6 +68,38 @@ public class Sportsman {
         }
     }
 
+    public void advices (double bmi, int age) {
+       if (isMale)  {
+        if (age > 20 && bmi > 18.5 && bmi < 24.9) {
+            System.out.println("For you it’s ideal to sleep 8 hours, " +
+                    "walk 10 000 steps, be outside for a 2 hours.");
+        }
+        else if (age > 40 && bmi > 18.5 && bmi < 24.9) {
+            System.out.println("For you it’s ideal to sleep 7 hours," +
+                    " walk 9 000 steps, be outside for a 1,5 hours.");
+        }
+        else if (age > 50 && bmi > 18.5 && bmi < 24.9) {
+            System.out.println("For you it’s ideal to sleep 8 hours, " +
+                    "walk 12 000 steps, be outside for an 3 hours.");
+        }
+    }
+       else if (!isMale) {
+           if (age > 20 && bmi > 19 && bmi < 25.2) {
+               System.out.println("For you it’s ideal to sleep 8 hours, " +
+                       "walk 10 000 steps, be outside for a 2 hours.");
+           }
+           else if (age > 40 && bmi > 19 && bmi < 25.2) {
+               System.out.println("For you it’s ideal to sleep 7 hours," +
+                       " walk 9 000 steps, be outside for a 1,5 hours.");
+           }
+           else if (age > 50 && bmi > 19 && bmi < 25.2) {
+               System.out.println("For you it’s ideal to sleep 8 hours, " +
+                       "walk 12 000 steps, be outside for an 3 hours.");
+           }
+       }
+
+       }
+
     public int getAge() {
         return age;
     }
