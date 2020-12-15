389b8503e6effc78170a955a8ef8c02197a4b3b5
Liene Dobele <liene2781@yahoo.com>
2020-12-15 17:50:47 +0200

    User input asked

--- src/com/company/Sportsman.java
+++ src/com/company/Sportsman.java
@@ -4,19 +4,23 @@ import java.util.Scanner;
 
 public class Sportsman {
 
+    private String name;
+    private int age;
+    private boolean isMale;
+    private double weightKg;
+    private double heightM;
+    private double bmi;
 
-    private static Scanner input = new Scanner(System.in);
+    public void setName(String name) {
+        this.name = name;
 
+    }
 
-    private String name = input.next();
-    private int age = input.nextInt();
-    private boolean isMale = input.nextBoolean();
-    private double weightKg = input.nextDouble();
-    private double heightM = input.nextDouble();
+    public Sportsman(String name, int age, double weightKg, double heightM) {
+        this.name = name;
+        this.age = age;
+        this.weightKg = weightKg;
+        this.heightM = heightM;
 
-
+    }
 }
-
-
-
-
