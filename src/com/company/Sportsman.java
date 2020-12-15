2315b846ac294ae8432b25d2f700a9a01bad8734
Liene Dobele <liene2781@yahoo.com>
2020-12-15 10:39:07 +0200

    Update Sportsman.java

--- src/com/company/Sportsman.java
+++ src/com/company/Sportsman.java
@@ -8,11 +8,12 @@ public class Sportsman {
     private static Scanner input = new Scanner(System.in);
 
 
+    private String name = input.next();
     private int age = input.nextInt();
     private boolean isMale = input.nextBoolean();
     private double weightKg = input.nextDouble();
     private double heightM = input.nextDouble();
-    private double bmi;
+
 
 }
 
