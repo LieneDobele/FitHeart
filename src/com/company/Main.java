49bde60a4a200ebdecbb1e59d2bbfa61745f7098
KristineTaluma <75748988+KristineTaluma@users.noreply.github.com>
2020-12-15 18:49:22 +0200

    Testing

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,28 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner input = new Scanner (System.in);
+
+
         System.out.println("Please, enter your name");
+        String name = input.next();
+        System.out.println("Please, enter your age");
+        int age = input.nextInt();
+        System.out.println("Please, enter your gender");
+        boolean isMale = input.nextBoolean();
+        System.out.println("Please, enter your weight");
+        double weightKg = input.nextDouble();
+        System.out.println("Please, enter your height");
+        double heightM = input.nextDouble();
+
+
+
+
 
 
 
--- src/com/company/Sportsman.java
+++ src/com/company/Sportsman.java
@@ -63,3 +63,4 @@ public class Sportsman {
             System.out.println("You are obese.");
 
         }
+    }
