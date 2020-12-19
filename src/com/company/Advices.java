d7dfec4dc2cbc72df48bd625c3cb7f03ef19ff77
Lidia Belyaeva <lvcaciag@gmail.com>
2020-12-19 13:09:22 +0200

    Removed unnecessary variables in waterAdvices method and argument weight from advices method. Added empty sout lines for extra spaces in output console

--- src/com/company/Advices.java
+++ src/com/company/Advices.java
@@ -4,7 +4,7 @@ import java.text.DecimalFormat;
 
 public class Advices {
 
-    public static void advices (double bmi, int age, double weightKg) {
+    public static void advices (double bmi, int age) {
         if (age >= 20 && bmi > 18.5 && bmi < 29.9) { //I deleted male and woman differences because they're almost the same
             System.out.println("For you it’s ideal to sleep 8 hours, " +
                     "walk 10 000 steps, be outside for a 2 hours.");
@@ -15,11 +15,10 @@ public class Advices {
             System.out.println("For you it’s ideal to sleep 8 hours, " +
                     "walk 8 000 steps, be outside for an 3 hours.");
         }
+        System.out.println();
     }
 
     public static void advicesWater(double weightKg, boolean isMale) {
-        weightKg = weightKg;
-        isMale = isMale;
         double waterIntakeMan = weightKg / 28;
         double waterIntakeWoman = weightKg / 30;
         DecimalFormat df1 = new DecimalFormat("#.##");
@@ -30,5 +29,6 @@ public class Advices {
             System.out.println("You should drink " + df1.format(waterIntakeWoman) + " liters of water per day.");
         }
         System.out.println("Follow a healthy diet, avoid sugar and refined carbohydrates.");
+        System.out.println();
     }
 }
